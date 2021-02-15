a7e1d3299fce424b748cf21dafa8b1e8d7bfbc52
Lawrence <lawrence-nowak@web.de>
2021-02-15 20:02:22 +0100

    set view engine from jade to ejs

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.listen(port, () => {
 })
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
