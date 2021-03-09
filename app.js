6c792552ed0e8a1d09af2ceb3feec9926f43d5e0
Lawrence <lawrence-nowak@web.de>
2021-03-09 16:17:04 +0100

    working on connecting dashboard.ejs to index.html via app.js and dashboard.js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+//const express = require('express');
+//const router = express.Router();
+
 function connect(){
         alert("app.js connected to public/index.html")
         console.log("app.js connected to public/index.html")
 };
 
+// function myRedirect(){
+//         for (redirect()=true){
+//                app.get("/dashboard", (req, res) => {
+//                        res.render("pages/dashboard");
+//                      })
+//                console.log("redirected to dashboard")
+//         }     
+// };
